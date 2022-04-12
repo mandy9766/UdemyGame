6563f60cdf0c307b2fd48e73e221ff8e4aff9416
ChoiTaesun <mandy9766@fgb.ai>
2022-04-12 14:15:41 +0900

    10강

---
+++ UdemyGame/Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("콘솔에 출력중입니다");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
